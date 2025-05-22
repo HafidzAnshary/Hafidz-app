c3a0454acf4fd6c71289b534a903b3c57eef967a
HafidzAnshary <abdulapis278@gmail.com>
2025-05-22 10:49:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🚀 Belajar Bikin Website Bareng Streamlit")
 st.write(
     "Yuk, ngoding santai sambil belajar bikin website interaktif pakai Python!."
 )
+st.image("file_00000000852c6230953c3e531b410fc2%20(1).png", width=200)
